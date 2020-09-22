1494b4611e3a92de0e60db048852c6b4ac5535af
Kimberly Seria <kseria@gms.ca>
2020-09-22 09:10:39 -0600

    getting relevant topics

--- src/main/java/gms/cims/bridge/MessageProcessor.java
+++ src/main/java/gms/cims/bridge/MessageProcessor.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class MessageProcessor implements Processor {
 
@@ -40,25 +41,11 @@ public class MessageProcessor implements Processor {
 
         JSONObject result = buildJsonObject(sourceData, data_before, data_after);
 
+        // build schema registry
         result.keys().forEachRemaining(key ->{
             System.out.println(key + ": " + result.get(key));
-            // record.set(key.toString(), result.getString(result.get(key).toString()));
+             // record.set(key, result.getString(result.get(key).toString()));
         });
-
-        // TO DO: BUILD SCHEMA FILE
-
-        // SET RECORD
-//        record.set("UserID", data_before.getString("UserID"));
-//        record.set("CO_ContractID", data_before.getInt("CO_ContractID"));
-//        // TO DO: SERIALIZE BYTES TYPE
-//        // record.set("RowVersion", data_before.get("RowVersion"));
-//        record.set("LastUpdate_before", data_before.get("LastUpdate"));
-//        record.set("IsIndividual_before", data_before.get("IsIndividual"));
-//        record.set("LastUpdate_after", data_after.get("LastUpdate"));
-//        record.set("IsIndividual_after", data_after.get("IsIndividual"));
-//        record.set("CCP_ID", data_before.get("CCP_ID"));
-//        record.set("CCP_AdminFee_before", data_before.get("CCP_AdminFee"));
-//        record.set("CCP_AdminFee_after", data_after.get("CCP_AdminFee"));
 
         return record.build();
     }
@@ -70,37 +57,29 @@ public class MessageProcessor implements Processor {
         source.keys().forEachRemaining(key -> {
             if(key.equals("schema") || key.equals("table")){
                 result.put(key, source.get(key));
-            }
-        });
-
-        // System.out.println("from source:" + result);
-
-        before.keys().forEachRemaining(key -> {
-            String holder = key + "_before";
-            Object value = before.get(key);
-            if(value instanceof String || value instanceof Integer || value instanceof Double || value instanceof Long || value instanceof Boolean || value instanceof Byte){
-                result.put(holder, value);
-            }
-        });
-
-        // System.out.println("from before:" + result);
-
-        after.keys().forEachRemaining(key -> {
-            String holder = key + "_after";
-            Object value = after.get(key);
-
-             if(value instanceof String || value instanceof Integer || value instanceof Double || value instanceof Long || value instanceof Boolean || value instanceof Byte){
-                if(after.has(key)){
-                    holder = key + "_after";
-//                    System.out.println(key + " After: " + value + " Before: " + before.get(key));
-
-                    if(!value.equals(before.get(key))){
-                        result.put(holder, value);
+                before.keys().forEachRemaining(beforeKey -> {
+                    Object value = before.get(beforeKey);
+                    if(value instanceof String || value instanceof Integer || value instanceof Double || value instanceof Long || value instanceof Boolean || value instanceof Byte){
+                        result.put(beforeKey, value);
                     }
-                }
-             }
+                });
+
+                after.keys().forEachRemaining(afterKey -> {
+                    String holder = afterKey + "_after";
+                    Object value = after.get(afterKey);
+
+                    if(value instanceof String || value instanceof Integer || value instanceof Double || value instanceof Long || value instanceof Boolean || value instanceof Byte){
+                        if(after.has(afterKey)){
+                            holder = afterKey + "_after";
+
+                            if(!value.equals(before.get(afterKey))){
+                                result.put(holder, value);
+                            }
+                        }
+                    }
+                });
+            }
         });
-        // System.out.println("from after:" + result);
 
         return result;
     }
