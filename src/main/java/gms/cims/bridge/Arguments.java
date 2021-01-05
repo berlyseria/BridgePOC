4ec01d95d510c07fa02794c3ddff55a3010f39d1
Kimberly Seria <kseria@gms.ca>
2021-01-05 14:38:30 -0600

    rever project to original camel-k

--- src/main/java/gms/cims/bridge/Arguments.java
+++ src/main/java/gms/cims/bridge/Arguments.java
@@ -2,7 +2,7 @@ package gms.cims.bridge;
 
 public class Arguments {
     public static String Broker ="172.16.86.23:29092";
-    public static String SourceTopic = "CIMSTEST.Customer.ClaimBlackList";
+    public static String SourceTopic = "CIMS.Customer.Groups";
     public static String DestinationTopic = "Test";
     // public static String DestinationTopic = "CIMSTEST.Test";
     public static String SchemaRegistry = "http://172.16.86.23:8081";
