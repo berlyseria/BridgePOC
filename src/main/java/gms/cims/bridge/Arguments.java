59c35da9eb031e6a635b2634a72099ffa13d9f45
Kimberly Seria <kseria@gms.ca>
2021-01-06 12:07:58 -0600

    updated argument values

--- src/main/java/gms/cims/bridge/Arguments.java
+++ src/main/java/gms/cims/bridge/Arguments.java
@@ -1,10 +1,10 @@
 package gms.cims.bridge;
 
 public class Arguments {
-    public static String Broker ="172.16.86.23:29092";
+    public static String Broker ="172.16.6.66:9092";
     public static String SourceTopic = "CIMS.Customer.Groups";
     public static String DestinationTopic = "Test";
     // public static String DestinationTopic = "CIMSTEST.Test";
-    public static String SchemaRegistry = "http://172.16.86.23:8081";
+    public static String SchemaRegistry = "http://172.16.6.66:8081";
     public static String GroupId = "cimstest";
 }
